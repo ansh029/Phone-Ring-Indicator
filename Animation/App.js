c2ed2145bff8d06894a183fdc5dafff7a503ca1d
anshuls4 <anshuls4@birlasoft.com>
2022-09-19 17:46:02 +0530

    Fixed Animation issues

--- Animation/App.js
+++ Animation/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {StyleSheet,View} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Feather';
+import { MotiView } from 'moti'
+import {Easing} from 'react-native-reanimated';
+
 
 const color='#6E01EF';
 const size=100;
@@ -12,12 +15,27 @@ const App= () => {
  
         <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
           <View style={[styles.dot,styles.center]}>
-          
-          <Icon name='phone-outgoing' size={32} color="#ffffff"/>
+            {[...Array(3).keys()].map(index=>{
+              return (
+              <MotiView
+              from={{opacity:0.7,scale:1}}
+              animate={{opacity:0,scale:4}}
+              transition={{
+                type:'timing',
+                duration:2000,
+                easing:Easing.out(Easing.ease),
+                delay:index*400,
+                loop:true,
+                repeatReverse:false
+              }}
+              key={index}
+              style={[StyleSheet.absoluteFillObject,styles.dot]}/>
+              );
+            })}
+           <Icon name='phone-outgoing' size={32} color="#ffffff"/>
           </View>
-      
         </View>
-    
+   
   );
 };
 
